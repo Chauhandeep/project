bae05f00240ff1a4e4231ac1f3e76d2076e7d0ea
Deepanshu <deepanshuchauhan.02@gmail.com>
2018-04-07 13:29:43 +0530

    Adding user schema and setting up login route

---
+++ server/models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+//when a login request is sent this method is called after saving
+//details in database which returns an access token to the client
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
+
+  user.tokens.unshift({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -4,13 +4,28 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose')
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
 
 //BodyParser is used for parsing json requests
 app.use(bodyParser.json());
+
+//Setting login route
+app.post('/login',(req,res) => {
+  var body = _.pick(req.body,['email','password']);
+  var user = new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
 
 //Server is run at localhost port 3000
 app.listen(port , () => {
