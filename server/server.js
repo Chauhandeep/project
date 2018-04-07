3a226c39b40881088367f7be76d8354d5b9f6782
Deepanshu <deepanshuchauhan.02@gmail.com>
2018-04-07 10:25:12 +0530

    Server is up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+const port = 3000;
+
+//BodyParser is used for parsing json requests
+app.use(bodyParser.json());
+
+//Server is run at localhost port 3000
+app.listen(port , () => {
+  console.log(`Server is up at port ${port}`);
+});
